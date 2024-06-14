ae707b4a6cf9bee64e6ad24d2297d8b9a2e664e2
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-06-14 19:28:17 +0900

    feat: [한재혁] Board Domain 생성 [BBZD-1]

---
+++ hotelbusterz/board/entity/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class Board(models.Model):
+    boardId = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128, null=False)
+    writer = models.CharField(max_length=32, null=False)
+    content = models.TextField()
+    regDate = models.DateTimeField(auto_now_add=True)
+    updDate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'board'
