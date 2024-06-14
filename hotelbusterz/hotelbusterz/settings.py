cb3be8d2e740d39c35e93cbae3247e268987b3d4
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-06-14 20:41:56 +0900

    add: [한재혁] settings.py에 빠진 부분 추가 [BBZD-1]

--- hotelbusterz/hotelbusterz/settings.py
+++ hotelbusterz/hotelbusterz/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
+    "rest_framework",
     "board",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
