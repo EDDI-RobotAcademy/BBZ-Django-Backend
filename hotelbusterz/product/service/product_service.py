6591dec3e616a5b961f38f4733a4a8a790db78d0
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-06-16 17:30:54 +0900

    feat: [한재혁] 호텔 정보를 DB에 등록 가능 [BBZD-7]

--- hotelbusterz/product/service/product_service.py
+++ hotelbusterz/product/service/product_service.py
@@ -5,3 +5,7 @@ class ProductService(ABC):
     @abstractmethod
     def list(self):
         pass
+
+    @abstractmethod
+    def createProduct(self, productName, productLocation, productActivity, productDining, productPrice, productImage):
+        pass
