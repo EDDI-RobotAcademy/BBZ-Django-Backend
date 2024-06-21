d4f16b17ecdcfdfd08ef739c59c38713a234ab9b
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-06-21 19:32:18 +0900

    feat: [한재혁] 게시판 게시글 조회 가능 [BBZD-9]

--- hotelbusterz/board/urls.py
+++ hotelbusterz/board/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('',  include(router.urls)),
     path('list/', BoardView.as_view({'get': 'list'}), name='board-list'),
     path('register', BoardView.as_view({'post': 'create'}), name='board-register'),
+    path('read/<int:pk>', BoardView.as_view({'get': 'read'}), name='board-read'),
 ]
