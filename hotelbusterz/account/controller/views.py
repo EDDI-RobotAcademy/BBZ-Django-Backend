68a583566805729f86d4351b43d7663293f5a9b7
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-06-24 20:38:35 +0900

    init: [이경민] account 도메인 초기 설정 [BBZD-13]

---
+++ hotelbusterz/account/controller/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+# Create your views here.
+class AccountView(viewsets.ViewSet):
+    pass
---
+++ hotelbusterz/account/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from account.controller.views import AccountView
+
+router = DefaultRouter()
+router.register(r'account', AccountView)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
