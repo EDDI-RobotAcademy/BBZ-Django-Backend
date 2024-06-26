6826cb3a955c5fff4da23dec5fb39ed7ce33a4af
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-06-26 18:23:05 +0900

    fix: [이경민] migrations 실행 오류 해결 [BBZD-13]

---
+++ hotelbusterz/account/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from account.entity.account import Account
+from account.entity.account_role_type import AccountRoleType
+from account.entity.profile import Profile
+
+# Create your models here.
