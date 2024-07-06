8da570746742d16782dc45e2155eedc9a7282b99
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-07-06 17:15:30 +0900

    fix: [한재혁] survey 데이터 추가할 때 오류 생기는 부분 해결 [BBZD-21]

--- hotelbusterz/survey/entity/models.py
+++ hotelbusterz/survey/entity/models.py
@@ -7,11 +7,11 @@ from product.entity.models import Product
 class Survey(models.Model):
     id = models.AutoField(primary_key=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
-    num_of_adult = models.PositiveIntegerField()
-    num_of_child = models.PositiveIntegerField()
-    have_breakfast = models.PositiveIntegerField()
-    is_exist_car = models.PositiveIntegerField()
-    len_of_reservation = models.PositiveIntegerField()
+    num_of_adult = models.PositiveIntegerField(default=1)
+    num_of_child = models.PositiveIntegerField(default=0)
+    have_breakfast = models.PositiveIntegerField(default=0)
+    is_exist_car = models.PositiveIntegerField(default=0)
+    len_of_reservation = models.PositiveIntegerField(default=1)
     payment_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
