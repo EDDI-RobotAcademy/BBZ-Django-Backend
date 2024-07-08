ab2224bb653c6657f38c30079a058eac9293ec78
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-07-08 16:31:00 +0900

    add: [한재혁] 계정 및 주문 더미 데이터 생성 가능 [BBZD-23]

---
+++ hotelbusterz/sample_data/generate_accounts_and_orders.py
@@ -0,0 +1,55 @@
+import random
+
+from django.db import transaction
+
+from account.entity.account import Account
+from account.entity.profile import Profile
+from orders.entity.orders import Orders
+from orders.entity.orders_item import OrdersItem
+from orders.entity.orders_status import OrderStatus
+from product.entity.models import Product
+
+products = list(Product.objects.all())
+account_ids = list(Account.objects.values_list('id', flat=True))
+
+
+def create_account(login_type_id, role_type_id, nickname, email):
+    unique_nickname = nickname
+    count = 1
+    while Profile.objects.filter(nickname=unique_nickname).exists():
+        unique_nickname = f"{nickname}_{count}"
+        count +=1
+
+    account = Account.objects.create(loginType_id=login_type_id, roleType_id=role_type_id)
+    Profile.objects.create(id=account.id, nickname=unique_nickname, email=email, account_id=account.id)
+    return account.id
+
+
+def create_random_order(account_id):
+    try:
+        with transaction.atomic():
+            order = Orders.objects.create(account_id=account_id, status=OrderStatus.PENDING)
+
+            product = random.choice(products)
+
+            OrdersItem.objects.create(
+                orders_id=order.id,
+                product_id=product.productId,
+            )
+
+    except Exception as e:
+        print(f"Error creating order for account {account_id}: {e}")
+
+
+if len(account_ids) < 10:
+    for i in range(len(account_ids), 10):
+        nickname = f"User{i + 1}"
+        email = f"user{i + 1}@example.com"
+        account_id = create_account(1, 1, nickname, email)
+        account_ids.append(account_id)
+
+for _ in range(10000):
+    account_id = random.choice(account_ids)
+    create_random_order(account_id)
+
+print("Sample data generation completed.")
